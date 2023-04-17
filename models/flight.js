452e0afff482504f444968e9aa79d47e9c7d3991
EleahBurman <eleahjburman@gmail.com>
2023-04-17 00:20:16 -0400

    New flight form

--- models/flight.js
+++ models/flight.js
@@ -5,16 +5,20 @@ const Schema = mongoose.Schema
 const departs = function(){
   const currentDate = new Date(Date.now())
   const departDate = new Date()
-  departDate.setFullYear(getFullYear() +1)
-  return departDate
+  departDate.setFullYear(currentDate +1)
+  departTime = departDate.setHours() + departDate.setMinutes()
+  
+  return departDate + ' ' + departTime
 }
 
 const flightSchema = new Schema({
   airline: {type: String, enum: ['American', 'Southwest', 'United']},
   airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN'},
   flightNo: {type: Number, required: (10 - 9999)},
-  departs: {type: Date, default: departs, timestamps: true}
-})
+  departs: {type: Date, default: departs}, 
+},
+  {timestamps: true})
+
 
 
 const Flight = mongoose.model('Flight', flightSchema)
--- routes/flights.js
+++ routes/flights.js
@@ -2,12 +2,18 @@ import { Router } from 'express'
 import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
-// GET localhost:3000/users
-router.get('/', function(req, res) {
-  res.send('respond with a resource')
-})
+// GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
 // GET localhost:3000/flights/new
 router.get('/new', flightsCtrl.new)
+// GET localhost:3000/flights/:flightId
+router.get('/:flightId', flightsCtrl.show)
+// GET localhost:3000/flights/:flightsId/edit
+router.get('/:flightId/edit', flightsCtrl.edit)
 // POST localhost:3000/flights
 router.post('/', flightsCtrl.create)
+// DELETE localhost:3000/movies/:movieId
+router.delete('/:flightId', flightsCtrl.delete)
+// PUT localhost:3000/movies/:movieId
+router.put('/:flightId', flightsCtrl.update)
 export { router }
