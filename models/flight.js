854ea8c0261176c9526c98e7b408868b7d41c376
EleahBurman <eleahjburman@gmail.com>
2023-04-16 22:03:37 -0400

    Model default in place

--- models/flight.js
+++ models/flight.js
@@ -2,9 +2,22 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const oneYearLater = () => {
+  const date = new Date()
+  date.setFullYear(date.getFullYear()+1)
+  return date
+}
+
 const flightSchema = new Schema({
   airline: {type: String, enum: ['American', 'Southwest', 'United']},
-  airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN']},
+  airport: {type: String, enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'], default: 'DEN'},
   flightNo: {type: Number, required: (10 - 9999)},
-  departs: {type: Date}
+  departs: {type: Date, default: oneYearLater}
 })
+
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+  Flight
+}
